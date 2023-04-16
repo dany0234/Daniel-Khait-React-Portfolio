1136927a2c3ef4d26fd6efe71ed389f2caa169de
vladi <vladiduynoviesf@gmail.com>
2023-04-16 21:57:15 +0300

    sidebar
    navigation bar completed

---
+++ react-portfolio/src/components/Layout/index.js
@@ -0,0 +1,25 @@
+import Sidebar from '../Sidebar';
+import './index.scss';
+
+const Layout = () => {
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;Body&gt;</span>
+                    
+                
+
+            </div>
+
+
+
+
+        </div>
+    ) 
+   
+
+}
+
+
+export default Layout;
